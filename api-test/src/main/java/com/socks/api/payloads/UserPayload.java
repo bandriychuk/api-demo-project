506962080261ca6e04be29882df2512237c3a87d
Andriychuk Borys <andriychuk23@gmail.com>
2021-03-20 23:14:17 +0200

    Created UserPayload

--- api-test/src/main/java/com/socks/api/payloads/UserPayload.java
+++ api-test/src/main/java/com/socks/api/payloads/UserPayload.java
@@ -1,7 +1,11 @@
 package com.socks.api.payloads;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jdk.nashorn.internal.objects.annotations.Setter;
 
+import javax.annotation.Generated;
+
+@Generated("com.robohorse.robopojogenerator")
 public class UserPayload{
 
 	@JsonProperty("password")
@@ -13,37 +17,4 @@ public class UserPayload{
 	@JsonProperty("username")
 	private String username;
 
-	public void setPassword(String password){
-		this.password = password;
-	}
-
-	public String getPassword(){
-		return password;
-	}
-
-	public void setEmail(String email){
-		this.email = email;
-	}
-
-	public String getEmail(){
-		return email;
-	}
-
-	public void setUsername(String username){
-		this.username = username;
-	}
-
-	public String getUsername(){
-		return username;
-	}
-
-	@Override
- 	public String toString(){
-		return 
-			"UserPayload{" + 
-			"password = '" + password + '\'' + 
-			",email = '" + email + '\'' + 
-			",username = '" + username + '\'' + 
-			"}";
-		}
 }
